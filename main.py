aeddb274484d598561de3c66f33bbbabc705ceb8
tzheng-7 <tiantianz258@deltalearns.ca>
2025-02-26 18:15:50 +0000

    Added basic code that asks user for guests' names & generates invites
    - Created empty list
    - Created variable that stores user's name
    - Created variable that stores the # of guests user wants to invite
    - Created loop that adds names to list till max # by tracking the #
    - Added loop that prints out invitations for each guest

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 '''This program will take user input about dinner guests and print invitations for each person'''
 
-# Ask user for # of guests they want to invite
+# Create empty list
+guestlist = []
 
-# Create loop that'll add names to list till #
+# Greet user
+username = input("\nHello! Hope you're having a great day <3\n\nWhat's your name?\n\n> ").strip().title()
+
+# Ask user for # of guests they want to invite
+numofguests = int(input(f"\nNice to meet you, {username}! How many guests would you like invitations to be generated for? (Please enter a valid whole number) \n\n> ").strip())
+
+# Create loop that'll add names to list till max #
+maxnumofguests = numofguests # Track # of guests added
+
+while maxnumofguests > 0: # While max isn't reached yet
+    newguest = input("\nPlease enter the name of the person you would like to invite.\n\n> ").title().strip()
+    guestlist.append(newguest) # Add guest to list
+    maxnumofguests -= 1 # Track # of guests added
 
 # Print invitation for each person
+for guest in guestlist:
+    print(f"\n\nYou're Invited!\n\nDear {guest},\n\nI would love for you to join me for a delightful evening of good food and great company. Let’s share a meal and make some wonderful memories together.\n\nPlease let me know if you can make it. I look forward to seeing you there!\n\nWarmly,\n\n{username}\n")
 
 # Ask user if they want to replace someone
 
-# Ask user for name of guest they want to replace & whom they want to replace them w
+# If yes, ask user for name of guest they want to replace & whom they want to replace them w
 
 # Regenerate invitations
+
+# If no, exit program
