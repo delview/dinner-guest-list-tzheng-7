531e320009c78b4b3e8dfa9ed7e7d2cdfbad79dd
tzheng-7 <tiantianz258@deltalearns.ca>
2025-02-26 17:42:17 +0000

    Added basic pseudocode

---
+++ main.py
@@ -0,0 +1,13 @@
+'''This program will take user input about dinner guests and print invitations for each person'''
+
+# Ask user for # of guests they want to invite
+
+# Create loop that'll add names to list till #
+
+# Print invitation for each person
+
+# Ask user if they want to replace someone
+
+# Ask user for name of guest they want to replace & whom they want to replace them w
+
+# Regenerate invitations
