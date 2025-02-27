d467f16068c2ec222974fb06a10436bd8dfef028
tzheng-7 <tiantianz258@deltalearns.ca>
2025-02-27 20:54:50 +0000

    Added code that replaces guest w new guest user chooses and regnerates invitations
    - Added code that asks for the replacement guest and adds that person to list
    - Added loop that regenerates all invitations in alphebetical order

--- main.py
+++ main.py
@@ -30,12 +30,16 @@ while replace == 'y':
     replacedguest = input("\nPlease enter the name of the person you would like to replace.\n\n> ").title().strip()
     if replacedguest in guestlist:
         guestlist.remove(replacedguest)
+        newguest = input(f"\nPlease enter the name of the person you would like to invite instead of {replacedguest}.\n\n> ").title().strip()
+        guestlist.append(newguest)
+        for i in range(len(guestlist)): # Regenerate invitations
+            guestlist.sort() # Print invites in aphabetical order
+            print(f"\n\nYou're Invited!\n\nDear {guestlist[i]},\n\nI would love for you to join me for a delightful evening of good food and great company. Let’s share a meal and make some wonderful memories together.\n\nPlease let me know if you can make it. I look forward to seeing you there!\n\nWarmly,\n\n{username}\n")
+        replace = input("\nWould you like to replace anyone? [Y]es/[N]o\n\n> ").strip().lower()
     else:
         print("\nSorry, that person is not on the list.")
 
-# Regenerate invitations
-
 # If no, exit program
 while replace == 'n':
-    print("\nHave a nice day <3")
+    print("\nHave a nice day <3\n")
     break
